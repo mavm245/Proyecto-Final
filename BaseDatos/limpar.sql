526b2c8374fdea7f62601f6fcb37b4094633aadd
mvasquezbec <mavmfi@hotmail.com>
2015-10-15 19:28:41 -0500

    Create limpar.sql

---
+++ BaseDatos/limpar.sql
@@ -0,0 +1,8 @@
+--Borrar informacion de la base de datos
+delete from manejador_db;
+delete from url_web;
+delete from puertos_web;
+delete from contacto;
+delete from solicitud;
+delete from sitio_web;
+delete from servidor_web;
